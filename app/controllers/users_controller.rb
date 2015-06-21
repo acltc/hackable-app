7ddd13d373c10a36b59bea68308fd4e463f5f800
Jay Wengrow <jaywngrw@gmail.com>
2015-06-21 10:13:37 -0500

    solution

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to user_path(@user.id), notice: 'User was successfully created.'
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
 
-    if @user.update(params[:user])
+    if @user.update(user_params)
       redirect_to user_path(@user.id), notice: 'User was successfully updated.'
     else
       render :edit
@@ -40,6 +40,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user.destroy
     redirect_to users_path, notice: 'User was successfully destroyed.'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name)
   end
 
 end
